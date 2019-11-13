9b347661a3b10d9f0afce3f6612d6b08a8226bb9
adrianval96 <adrianval96@gmail.com>
2019-11-13 23:34:34 +0100

    first commit

---
+++ projects/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from projects.models import Project
+
+# Performs a query and returns all objects in database
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
+
+# Performs a query and returns ONE searched object from the database
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
