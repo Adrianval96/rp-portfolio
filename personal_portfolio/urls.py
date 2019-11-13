099573f26d5b3987aa91cb1120de9fcc9f43c6e8
adrianval96 <adrianval96@gmail.com>
2019-11-14 00:46:37 +0100

    Added blog functionality

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -21,7 +21,8 @@ urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     #path('projects/', include('projects.urls')),
     #url(r'^projects/', include('projects.urls')),
-    path("", include("projects.urls")),
+    #path("", include("projects.urls")),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
 ]
